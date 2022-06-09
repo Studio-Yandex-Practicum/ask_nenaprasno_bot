c4b8370cb022e1cee25f5a58dadf1bba1c72a776
Денис <danielseddian@yandex.ru>
2022-06-09 19:39:18 +0300

    Bot base 20.0a0

--- tools/bot.py
+++ tools/bot.py
@@ -8,9 +8,9 @@ from os_tools import get_secret
 
 LOGGER = get_logger("Bot")
 # --------------------------------------------------------------------------------------------- #
-# Настройки webhook'а telegram bot'а                                                            #
-# https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks                      #
 # Следует создать и указать в config.py                                                         #
+# настройки webhook'а telegram bot'а                                                            #
+# https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks                      #
 # --------------------------------------------------------------------------------------------- #
 TOKEN = get_secret("TELEGRAM_TOKEN")
 LISTEN_URL = "127.0.0.1"
