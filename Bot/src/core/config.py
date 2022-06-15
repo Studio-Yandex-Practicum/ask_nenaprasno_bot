38395f1d2d7b7a21df9b3ed90ca801666c88353c
Денис <DanielSeddian@yandex.ru>
2022-06-15 11:49:01 +0300

    Updated bot running via uvicorn

--- Bot/src/core/config.py
+++ Bot/src/core/config.py
@@ -3,13 +3,13 @@ from dotenv import dotenv_values
 # --------------------------------------------------------------------------------- #
 # Getting variables from .env                                                       #
 # --------------------------------------------------------------------------------- #
-ENVS = dotenv_values()
+env = dotenv_values()
 
-HOST = ENVS["HOST"]
-WEBHOOK_URL = ENVS["WEBHOOK_URL"]
-PORT = int(ENVS["BOT_PORT"])
-TOKEN = ENVS["TELEGRAM_TOKEN"]
-WEBHOOK_IP = ENVS["WEBHOOK_IP"]
-CHAT_ID = ENVS["CHAT_ID"]
-TEST_PERIOD = int(ENVS["TEST_PERIOD"])
+HOST = env.get("HOST")
+WEBHOOK_URL = env.get("WEBHOOK_URL")
+PORT = int(env.get("BOT_PORT"))
+TOKEN = env.get("TELEGRAM_TOKEN")
+WEBHOOK_IP = env.get("WEBHOOK_IP")
+CHAT_ID = env.get("CHAT_ID")
+TEST_PERIOD = int(env.get("TEST_PERIOD"))
 # --------------------------------------------------------------------------------- #
