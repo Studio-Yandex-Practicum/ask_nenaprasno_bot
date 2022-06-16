95489fc6fa2bb68381f9376a3d0341aed893d6ed
Nikita Kovalevskiy <koval701@gmail.com>
2022-06-16 12:09:37 +0700

    Create functionality of delay button

---
+++ Bot/src/service/delay.py
@@ -0,0 +1,38 @@
+# Use case
+# CallbackQueryHandler(delay_message_for_1_hour)
+
+from datetime import timedelta
+
+from telegram import Update
+from telegram.ext import ContextTypes, CallbackContext
+
+
+async def delay_message_for_1_hour(
+        update: Update,
+        context: ContextTypes.DEFAULT_TYPE
+) -> None:
+    """
+    Add a repeat message task to the queue.
+    """
+    query = update.callback_query
+    data = query.message
+    context.job_queue.run_once(
+        callback=repeat_message,
+        when=timedelta(hours=1),
+        data=data
+    )
+    await query.answer()  # close progress bar in chat
+
+
+async def repeat_message(
+        context: CallbackContext
+) -> None:
+    """
+    Repeat delayed message.
+    """
+    data = context.job.data
+    await context.bot.send_message(
+        chat_id=data.chat_id,
+        text=data.text_markdown_v2_urled,
+        reply_markup=data.reply_markup
+    )
