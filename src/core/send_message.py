5f41ff3cb301cff882f3e062b5630ee5d406bc5e
Evgenia Agamova <evgenia@agamova.ru>
2022-06-22 23:32:17 +0300

    Added Docker Support.

--- src/core/send_message.py
+++ src/core/send_message.py
@@ -1,34 +1,24 @@
 import logging
 
+from telegram import ReplyKeyboardMarkup
 from telegram.error import TelegramError
 from telegram.ext import CallbackContext
-from telegram import ReplyKeyboardMarkup
 
 
 async def send_message(
-    context: CallbackContext,
-    chat_id: int,
-    text: str,
-    reply_markup: ReplyKeyboardMarkup | None = None
+    context: CallbackContext, chat_id: int, text: str, reply_markup: ReplyKeyboardMarkup | None = None
 ) -> bool:
     """
     Send simple text message.
     :param context: CallbackContext
     :param chat_id: int
     :param text: str
-    :param reply_mark_up: ReplyKeyboardMarkup | None
+    :param reply_markup: ReplyKeyboardMarkup | None
     """
     try:
-        await context.bot.send_message(
-            chat_id=chat_id,
-            text=text,
-            reply_markup=reply_markup
-        )
+        await context.bot.send_message(chat_id=chat_id, text=text, reply_markup=reply_markup)
         return True
 
-    except TelegramError as error:
-        logging.exception(
-            f'The error sending the message to the chat: {chat_id}',
-            error
-        )
+    except TelegramError:
+        # logging.exception("The error sending the message to the chat: %d " % chat_id, error)
         return False
