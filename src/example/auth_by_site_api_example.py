af83eb81e1aa23b26addc395158016a97697ecd8
Nikita Kovalevskiy <koval701@gmail.com>
2022-06-25 22:56:44 +0700

    refactored and add docstring

--- src/example/auth_by_site_api_example.py
+++ src/example/auth_by_site_api_example.py
@@ -1,33 +1,37 @@
 from telegram import Update
-from telegram.ext import CommandHandler, ContextTypes, PicklePersistence, ApplicationBuilder
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, PicklePersistence
 
-from core.send_message import send_message
 from service.auth_procedure import auth_telegram_user
 from src.core.config import TOKEN
 
 
-async def get_from_persistance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    user_id = update.message.from_user.id
-    user_data = context.user_data["user_name"]
-    await update.message.reply_text(f"try get data from persistence of {user_data}")
+async def get_from_persistence(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_name = context.user_data.get("user_name")
+    user_id_in_trello = context.user_data.get("user_id_in_trello")
+    user_time_zone = context.user_data.get("user_time_zone")
+    if not user_name and not user_id_in_trello and not user_time_zone:
+        await update.message.reply_text("bot don't have the data in persistence")
+    else:
+        await update.message.reply_text(
+            f"user_data: {user_name}\nuser_id_in_trello: {user_id_in_trello}\nuser_time_zone: {user_time_zone}"
+        )
 
 
-async def push_to_persistance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def push_to_persistence(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_id = update.message.from_user.id
-    await auth_telegram_user(
-        telegram_id=user_id,
-        context=context
-    )
-    await update.message.reply_text(f"try push data to persistence of {user_id}")
+    done = await auth_telegram_user(telegram_id=user_id, context=context)
+    if done:
+        await update.message.reply_text("Succeed get data from site api and write to persistence file")
+    else:
+        await update.message.reply_text("Something went wrong")
 
 
 def main() -> None:
     """Run the bot."""
-
     bot_persistence = PicklePersistence(filepath="test_persistence_file")
     application = ApplicationBuilder().token(TOKEN).persistence(persistence=bot_persistence).build()
-    application.add_handler(CommandHandler("get", get_from_persistance))
-    application.add_handler(CommandHandler("push", push_to_persistance))
+    application.add_handler(CommandHandler("get", get_from_persistence))
+    application.add_handler(CommandHandler("push", push_to_persistence))
     application.run_polling()
 
 
--- src/service/auth_procedure.py
+++ src/service/auth_procedure.py
@@ -1,18 +1,20 @@
-from telegram import Update
 from telegram.ext import ContextTypes
 
 from src.service import ConreateAPIService
 
 
-async def auth_telegram_user(
-    telegram_id: int,
-    context: ContextTypes.DEFAULT_TYPE
-):
-    """context.bot_data[auth_status.user_id_in_trello] = update.effective_chat.id"""
+async def auth_telegram_user(telegram_id: int, context: ContextTypes.DEFAULT_TYPE):
+    """
+    try to authenticate telegram user on site API and write trello_id to persistence file
+    :param telegram_id: user's id from telegram
+    :param context: context
+    :return: status of authentication
+    """
     api_service = ConreateAPIService()
     user_data = await api_service.authenticate_user(telegram_id=telegram_id)
+    if user_data is None:
+        return False
     context.user_data["user_name"] = user_data.user_name
     context.user_data["user_id_in_trello"] = user_data.user_id_in_trello
     context.user_data["user_time_zone"] = user_data.user_time_zone
-
-
+    return True
