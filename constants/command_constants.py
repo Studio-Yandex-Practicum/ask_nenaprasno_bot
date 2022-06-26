00e01c2eb3d57d709046f9ae3fb5365e14f8f1a0
SERGEY ZHAROV <zhss.83@yandex.ru>
2022-06-26 20:26:13 +0300

    Deleted unused constants

--- constants/command_constants.py
+++ constants/command_constants.py
@@ -5,11 +5,7 @@ STATISTIC_MONTH_COMMAND = "month_statistic_command"
 STATISTIC_WEEK_COMMAND = "week_statitstic_command"
 ACTUAL_REQUESTS_COMMAND = "actual_requests_command"
 OVERDUE_REQUESTS_COMMAND = "overdue_requests_command"
-
 REPEAT_COMMAND = "repeat_after_one_hour_command"
-SEND_SOON_COMMAND = "send_soon_command"  # Не используемая константа
-ALREADY_SEND_COMMAND = "already_send_command"  # Не используемая константа
-
 IS_EXPERT_COMMAND = "is_expert_command"
 NOT_EXPERT_COMMAND = "not_expert_command"
 REGISTR_AS_EXPERT_COMMAND = "registr_as_expert_command"
