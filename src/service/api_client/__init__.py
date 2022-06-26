924b71c2077cfa404db74f4d7e834eb64286cf09
andrey_murysev <andreimurysev@yandex.ru>
2022-06-26 19:41:36 +0300

    add new models

--- src/service/api_client/__init__.py
+++ src/service/api_client/__init__.py
@@ -1,8 +1,8 @@
-from core.config import FAKE_API
+from core.config import IS_FAKE_API
 from service.api_client.hemas import ShemasAPIService
 from service.api_client.mock_shemas import MockShemasAPIService
 
-if FAKE_API:
+if IS_FAKE_API:
     ConreateAPIService = MockShemasAPIService
 else:
     ConreateAPIService = ShemasAPIService
