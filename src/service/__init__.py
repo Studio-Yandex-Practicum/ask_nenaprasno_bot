215dfbaddeabdbeed1b812d4965392e0056d4e46
Nikita Kovalevskiy <koval701@gmail.com>
2022-06-26 10:51:33 +0700

    rename config constants and add comments

--- src/service/__init__.py
+++ src/service/__init__.py
@@ -1,8 +1,8 @@
-from core.config import FAKE_API
+from core.config import IS_FAKE_API
 from service.api_client_fake import FakeAPIService
 from service.api_client_real import RealAPIService
 
-if FAKE_API:
+if IS_FAKE_API:
     ConreateAPIService = FakeAPIService
 else:
     ConreateAPIService = RealAPIService
