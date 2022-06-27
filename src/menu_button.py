c7e2b295b7aad558e05909a3a02c350899552257
KaterinaSolovyeva <aka-malta@yandex.ru>
2022-06-27 11:48:22 +0300

    Fixed conflicts in PR

--- src/menu_button.py
+++ src/menu_button.py
@@ -5,6 +5,5 @@ from telegram.ext import CallbackContext
 async def menu_button(context: CallbackContext) -> None:
     command = [
         BotCommand("menu", "Показать список возможных запросов к боту"),
-        BotCommand("get_timezone", "⌚ Настроить часовой пояс"),
     ]
     await context.bot.set_my_commands(command)
