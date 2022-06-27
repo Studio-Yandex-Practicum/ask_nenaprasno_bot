b52aa479772e43f6b8d12429d2bbac9a019d1bef
Kapkaev Andrey <kapkaew@yandex.ru>
2022-06-28 00:47:30 +0300

    Add template messages.

--- src/service/send_mailing.py
+++ src/service/send_mailing.py
@@ -2,7 +2,6 @@ from string import Template
 from typing import Any, Optional, Union, List
 import logging
 
-from telegram.error import TelegramError
 from telegram.ext import CallbackContext
 from telegram import ReplyKeyboardMarkup
 
@@ -15,7 +14,16 @@ async def send_month_statistic(
     context: CallbackContext, reply_markup: Optional[ReplyKeyboardMarkup]
 ):
     mont_statistic_obj = await ConreateAPIService().get_month_stat()
-    message = Template('')
+    message = Template(
+        'Это был отличный месяц!\n'
+        'Посмотрите, как он прошел в *""Просто спросить""* 🔥\n\n'
+        'Количество закрытых заявок - *$user_tickets_closed*\n'
+        'Рейтинг - *$user_rating*\n'
+        'Среднее время ответа - *$user_ticket_resolve_avg_time*\n\n'
+        'Открыть [Trello](https://trello.com)\n\n'
+        'Мы рады работать в одной команде :)\n'
+        '*Так держать!*'
+    )
     await start_mailing_statistic(
         context=context, template_message=message,
         statistic=mont_statistic_obj.month_stat,
@@ -28,7 +36,17 @@ async def send_week_statistic(
     context: CallbackContext, reply_markup: Optional[ReplyKeyboardMarkup]
 ):
     week_statistic_obj = await ConreateAPIService().get_week_stat()
-    message = Template('')
+    message = Template(
+        'Вы делали добрые дела 7 дней!\n'
+        'Посмотрите, как прошла ваша неделя  в *""Просто спросить""*\n'
+        'Закрыто заявок - *$last_week_user_tickets_closed*\n'
+        'В работе *$last_week_user_tikets_in_work* заявок  за неделю\n\n'
+        'Истекает срок у *$last_week_user_tickets_expiring заявок*\n'
+        'У *$last_week_user_tickets_expired* заявок срок истек\n\n'
+        'Открыть [Trello](https://trello.com)\n\n'
+        'Мы рады работать в одной команде :)\n'
+        '*Так держать!*'
+    )
     await start_mailing_statistic(
         context=context, template_message=message,
         statistic=week_statistic_obj.week_stat,
