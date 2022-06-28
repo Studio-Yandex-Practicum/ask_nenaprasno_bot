e7aa0d1463c03be5f87c7dd5bc7e2e9954760448
Kapkaev Andrey <kapkaew@yandex.ru>
2022-06-28 15:46:27 +0300

    Replace send_statistic function to the core -> send_message.py

--- src/core/send_message.py
+++ src/core/send_message.py
@@ -1,9 +1,12 @@
-from typing import Any, Optional
+from typing import Any, Optional, List, Union
 import logging
+from string import Template
 
 from telegram.error import TelegramError
 from telegram.ext import CallbackContext
 from telegram import ReplyKeyboardMarkup
+
+from src.service.api_client_dataclasses import UserMonthStat, UserWeekStat
 
 
 async def send_message(
@@ -36,3 +39,28 @@ async def send_message(
             error
         )
         return False
+
+
+async def sending_statistics(
+    context: CallbackContext,
+    template_message: Template,
+    statistic: List[Union[UserMonthStat, UserWeekStat]],
+    parse_mode: Any = None,
+    reply_markup: Optional[ReplyKeyboardMarkup] = None,
+):
+    """
+    Start mailing message with statistics.
+    :param context: CallbackContext
+    :param template_message: Template
+    :param statistic: List[Union[UserMonthStat, UserWeekStat]]
+    :param parse_mode: Any = None
+    :param reply_markup: ReplyKeyboardMarkup | None
+    """
+    for user_statistic in statistic:
+        await send_message(
+            context=context,
+            chat_id=user_statistic.telegram_id,
+            text=template_message.substitute(**user_statistic.__dict__),
+            parse_mode=parse_mode,
+            reply_markup=reply_markup
+        )
