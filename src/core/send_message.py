0b1326698d47fc6a87e7453ef06039b8d7ee723c
Kapkaev Andrey <kapkaew@yandex.ru>
2022-06-28 22:47:49 +0300

    All logic relocate to jobs.py

--- src/core/send_message.py
+++ src/core/send_message.py
@@ -1,19 +1,17 @@
-from typing import Any, Optional, List, Union
+from typing import Optional
 import logging
-from string import Template
 
+from telegram.constants import ParseMode
 from telegram.error import TelegramError
 from telegram.ext import CallbackContext
 from telegram import ReplyKeyboardMarkup
-
-from src.service.api_client_dataclasses import UserMonthStat, UserWeekStat
 
 
 async def send_message(
     context: CallbackContext,
     chat_id: int,
     text: str,
-    parse_mode: Any = None,
+    parse_mode: str = ParseMode.MARKDOWN,
     reply_markup: Optional[ReplyKeyboardMarkup] = None
 ) -> bool:
     """
@@ -39,28 +37,3 @@ async def send_message(
             error
         )
         return False
-
-
-async def send_statistics(
-    context: CallbackContext,
-    template_message: Template,
-    statistic: List[Union[UserMonthStat, UserWeekStat]],
-    parse_mode: Any = None,
-    reply_markup: Optional[ReplyKeyboardMarkup] = None,
-):
-    """
-    Start mailing message with statistics.
-    :param context: CallbackContext
-    :param template_message: Template
-    :param statistic: List[Union[UserMonthStat, UserWeekStat]]
-    :param parse_mode: Any = None
-    :param reply_markup: ReplyKeyboardMarkup | None
-    """
-    for user_statistic in statistic:
-        await send_message(
-            context=context,
-            chat_id=user_statistic.telegram_id,
-            text=template_message.substitute(**user_statistic.__dict__),
-            parse_mode=parse_mode,
-            reply_markup=reply_markup
-        )
