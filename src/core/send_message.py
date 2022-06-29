887457568a6147c07d23d2fea87e8e00c64c13a5
Kapkaev Andrey <kapkaew@yandex.ru>
2022-06-29 23:02:57 +0300

    Little refactoring, relocate all logic in send_message.py

--- src/core/send_message.py
+++ src/core/send_message.py
@@ -1,11 +1,13 @@
-from typing import Optional
+from string import Template
+from typing import Optional, List, Union
 import logging
-
 
 from telegram.constants import ParseMode
 from telegram.error import TelegramError
 from telegram.ext import CallbackContext
 from telegram import ReplyKeyboardMarkup
+
+from src.service.api_client_dataclasses import UserMonthStat, UserWeekStat
 
 
 async def send_message(
@@ -35,3 +37,42 @@ async def send_message(
     except TelegramError as error:
         logging.exception(("The error sending the message to the chat: %s", chat_id), error)
         return False
+
+
+async def send_statistics(
+    context: CallbackContext,
+    template_message: Template,
+    template_attribute_aliases: dict,
+    statistic: List[Union[UserMonthStat, UserWeekStat]],
+    reply_markup: Optional[ReplyKeyboardMarkup] = None,
+):
+    """
+    Start mailing message with statistics.
+    :param context: CallbackContext
+    :param template_message: Template
+    :param template_attribute_aliases: dict in this dictionary,
+        the keys are the names of attributes in the message
+        template and the keys are the names of attributes in
+        the data object.
+    :param statistic: List[Union[UserMonthStat, UserWeekStat]]
+    :param reply_markup: ReplyKeyboardMarkup | None
+    """
+    for user_statistic in statistic:
+        if user_statistic.username not in context.bot_data['username_to_id']:
+            logging.warning(
+                f'User with username {user_statistic.username} '
+                f'not found in bot data. Maybe user not run bot '
+                f'or not authenticated in bot')
+            continue
+        chat_id = context.bot_data['username_to_id'][user_statistic.username]
+        message = template_message.substitute(
+            {key: getattr(user_statistic, attribute)
+             for key, attribute in template_attribute_aliases.items()}
+        )
+        await send_message(
+            context=context,
+            chat_id=chat_id,
+            text=message,
+            reply_markup=reply_markup
+        )
+
--- src/jobs.py
+++ src/jobs.py
@@ -1,9 +1,8 @@
 from string import Template
-import logging
 
 from telegram.ext import CallbackContext
 
-from src.core.send_message import send_message
+from src.core.send_message import send_statistics
 from src.service import ConreateAPIService
 
 
@@ -12,36 +11,27 @@ async def weekly_stat_job(context: CallbackContext) -> None:
     Send weekly statistics on the number of requests in the work
     """
     week_statistic_obj = await ConreateAPIService().get_week_stat()
-    pre_message = Template(
+    template_message = Template(
         'Вы делали добрые дела 7 дней!\n'
         'Посмотрите, как прошла ваша неделя  в *""Просто спросить""*\n'
         'Закрыто заявок - *$tickets_closed*\n'
-        'В работе *$tikets_in_work* заявок  за неделю\n\n'
+        'В работе *$tickets_in_work* заявок  за неделю\n\n'
         'Истекает срок у *$tickets_expiring заявок*\n'
         'У *$tickets_expired* заявок срок истек\n\n'
         'Открыть [Trello](https://trello.com)\n\n'
         'Мы рады работать в одной команде :)\n'
         '*Так держать!*'
     )
-    for user_statistic in week_statistic_obj.week_stat:
-        if user_statistic.username not in context.bot_data['username_to_id']:
-            logging.warning(
-                f'User with username {user_statistic.username} '
-                f'not found in bot data. Maybe user not run bot '
-                f'or not authenticated in bot')
-            continue
-        chat_id = context.bot_data['username_to_id'][user_statistic.username]
-        message = pre_message.substitute(
-            tickets_closed=user_statistic.last_week_user_tickets_closed,
-            tikets_in_work=user_statistic.last_week_user_tikets_in_work,
-            tickets_expiring=user_statistic.last_week_user_tickets_expiring,
-            tickets_expired=user_statistic.last_week_user_tickets_expired,
-        )
-        await send_message(
-            context=context,
-            chat_id=chat_id,
-            text=message,
-        )
+    alias_dict = dict(
+        tickets_closed='last_week_user_tickets_closed',
+        tickets_in_work='last_week_user_tikets_in_work',
+        tickets_expiring='last_week_user_tickets_expiring',
+        tickets_expired='last_week_user_tickets_expired'
+    )
+    await send_statistics(
+        context, template_message, alias_dict,
+        week_statistic_obj.week_stat,
+    )
 
 
 async def monthly_receipt_reminder_job(context: CallbackContext) -> None:
@@ -58,7 +48,7 @@ async def monthly_stat_job(context: CallbackContext) -> None:
     Only if the user had requests
     """
     mont_statistic_obj = await ConreateAPIService().get_month_stat()
-    pre_message = Template(
+    template_message = Template(
         'Это был отличный месяц!\n'
         'Посмотрите, как он прошел в *""Просто спросить""* 🔥\n\n'
         'Количество закрытых заявок - *$tickets_closed*\n'
@@ -68,21 +58,12 @@ async def monthly_stat_job(context: CallbackContext) -> None:
         'Мы рады работать в одной команде :)\n'
         '*Так держать!*'
     )
-    for user_statistic in mont_statistic_obj.month_stat:
-        if user_statistic.username not in context.bot_data['username_to_id']:
-            logging.warning(
-                f'User with username {user_statistic.username} '
-                f'not found in bot data. Maybe user not run bot '
-                f'or not authenticated in bot')
-            continue
-        chat_id = context.bot_data['username_to_id'][user_statistic.username]
-        message = pre_message.substitute(
-            tickets_closed=user_statistic.user_tickets_closed,
-            rating=user_statistic.user_rating,
-            ticket_resolve_avg_time=user_statistic.user_ticket_resolve_avg_time,
-        )
-        await send_message(
-            context=context,
-            chat_id=chat_id,
-            text=message,
-        )
+    alias_dict = dict(
+        tickets_closed='user_tickets_closed',
+        rating='user_rating',
+        ticket_resolve_avg_time='user_ticket_resolve_avg_time',
+    )
+    await send_statistics(
+        context, template_message, alias_dict,
+        mont_statistic_obj.month_stat,
+    )
