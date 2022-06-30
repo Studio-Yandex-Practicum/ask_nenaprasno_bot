75a57c4e44f06b59500868d4b4cafddc3614b177
Kapkaev Andrey <kapkaew@yandex.ru>
2022-06-30 10:42:30 +0300

    rename telegram_name(str) to telegram_id(int) fix little mistakes.

--- src/service/api_client/mock_api_service.py
+++ src/service/api_client/mock_api_service.py
@@ -1,4 +1,5 @@
 from http import HTTPStatus
+from typing import Optional
 
 from service.api_client.base import AbstractAPIService
 from service.api_client.models import BillStat, MonthStat, UserData, UserMonthStat, UserWeekStat, WeekStat
@@ -6,27 +7,27 @@ from service.api_client.models import BillStat, MonthStat, UserData, UserMonthSt
 
 class MockAPIService(AbstractAPIService):
     async def get_bill(self) -> BillStat:
-        return BillStat(telegram_name=["user1@telegram", "user2@telegram", "user3@telegram"])
+        return BillStat(telegram_ids=[12345685, 78945656, 4564512312])
 
     async def get_week_stat(self) -> list[WeekStat]:
         return [
             WeekStat(
-                telegram_name="user1@telegram",
+                telegram_id=971746479,
                 user_time_zone="UTC+3",
                 user_name_in_trello="user1@trello",
                 last_week_user_tickets_closed=10,
-                last_week_user_tickets_not_expring=2,
+                last_week_user_tickets_not_expiring=2,
                 last_week_user_tickets_expiring=1,
                 last_week_user_tickets_expired=1,
                 last_week_user_tickets_in_work=4,
                 last_week_user_tickets_all=14,
             ),
             WeekStat(
-                telegram_name="user2@telegram",
+                telegram_id=721889325,
                 user_time_zone="UTC+3",
                 user_name_in_trello="user2@trello",
                 last_week_user_tickets_closed=5,
-                last_week_user_tickets_not_expring=3,
+                last_week_user_tickets_not_expiring=3,
                 last_week_user_tickets_expiring=2,
                 last_week_user_tickets_expired=1,
                 last_week_user_tickets_in_work=6,
@@ -37,14 +38,14 @@ class MockAPIService(AbstractAPIService):
     async def get_month_stat(self) -> list[MonthStat]:
         return [
             MonthStat(
-                telegram_name="user1@telegram",
+                telegram_id=971746479,
                 user_time_zone="UTC+3",
                 user_tickets_closed=10,
                 user_rating=3.2,
                 user_ticket_resolve_avg_time=4.1,
             ),
             MonthStat(
-                telegram_name="user2@telegram",
+                telegram_id=721889325,
                 user_time_zone="UTC+3",
                 user_tickets_closed=5,
                 user_rating=2.2,
@@ -56,7 +57,7 @@ class MockAPIService(AbstractAPIService):
         return UserWeekStat(
             user_name_in_trello="user1@telegram",
             last_week_user_tickets_closed=10,
-            last_week_user_tickets_not_expring=2,
+            last_week_user_tickets_not_expiring=2,
             last_week_user_tickets_expiring=1,
             last_week_user_tickets_expired=1,
             last_week_user_tickets_in_work=4,
@@ -70,7 +71,7 @@ class MockAPIService(AbstractAPIService):
             user_ticket_resolve_avg_time=5.1,
         )
 
-    async def authenticate_user(self, telegram_name: str) -> UserData | None:
+    async def authenticate_user(self, telegram_id: int) -> Optional[None]:
         return UserData(
             user_name="Bob",
             user_time_zone="UTC+3",
