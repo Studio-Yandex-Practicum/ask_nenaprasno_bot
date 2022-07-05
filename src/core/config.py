f00b73720b9ebb833dc28ed1ac2666a3f95566d0
antonov-andreu <andreu-antonov@yandex.ru>
2022-07-05 17:27:10 +0300

    Refactoring env in config.py v2

--- src/core/config.py
+++ src/core/config.py
@@ -38,23 +38,30 @@ PORT = get_const_int("BOT_PORT")  # port для доступа к uvicorn сер
 TOKEN = get_const_string("TELEGRAM_TOKEN")  # Токен телеграм бота
 
 WEEKLY_STAT_TIME = get_const_datetime("WEEKLY_STAT_TIME")  # время еженедельной статистики
-WEEKLY_STAT_WEEK_DAYS = get_const_datetime_tuple("WEEKLY_STAT_WEEK_DAYS")  # дни недели для еженедельной статистики 0-6, где 0 - воскресенье
+WEEKLY_STAT_WEEK_DAYS = get_const_datetime_tuple(
+    "WEEKLY_STAT_WEEK_DAYS"
+)  # дни недели для еженедельной статистики 0-6, где 0 - воскресенье
 
-MONTHLY_STAT_TIME = get_const_datetime("MONTHLY_STAT_TIME") # время ежемесячной статистики
+MONTHLY_STAT_TIME = get_const_datetime("MONTHLY_STAT_TIME")  # время ежемесячной статистики
 MONTHLY_STAT_DAY = get_const_int("MONTHLY_STAT_DAY")  # день для даты ежемесячной статистики
 
-MONTHLY_RECEIPT_REMINDER_TIME = get_const_datetime("MONTHLY_RECEIPT_REMINDER_TIME")  # время для ежемесячного напоминания о чеке
-MONTHLY_RECEIPT_REMINDER_DAY = get_const_int("MONTHLY_RECEIPT_REMINDER_DAY")  # день для даты ежемесячного напоминания о чеке
+MONTHLY_RECEIPT_REMINDER_TIME = get_const_datetime(
+    "MONTHLY_RECEIPT_REMINDER_TIME"
+)  # время для ежемесячного напоминания о чеке
+MONTHLY_RECEIPT_REMINDER_DAY = get_const_int(
+    "MONTHLY_RECEIPT_REMINDER_DAY"
+)  # день для даты ежемесячного напоминания о чеке
 
 URL_SERVICE_RULES = env.get(
     "URL_SERVICE_RULES", "https://docs.google.com/document/d/1hW2HUv9aWQMnUBuIE_YQEtmIDDbk8KhpychckbyaIEQ/edit"
 )
 
 BOT_PERSISTENCE_FILE = get_const_string("BOT_PERSISTENCE_FILE")  # имя файла persistence бота
-IS_FAKE_API = get_const_string("IS_FAKE_API")  # флаг, определяющий какой АПИ клиент используется - боевой или "заглушка"
+IS_FAKE_API = get_const_string(
+    "IS_FAKE_API"
+)  # флаг, определяющий какой АПИ клиент используется - боевой или "заглушка"
 SITE_API_URL = get_const_string("SITE_API_URL")  # адрес сервера, к которому будет отправлять запросы АПИ клиент
 
 TRELLO_API_KEY = get_const_string("TRELLO_API_KEY")  # API ключ разработчика
 TRELLO_ID_MODEL = get_const_string("TRELLO_ID_MODEL")  # id таблицы, к которой будет привязан webhook
 TRELLO_TOKEN = get_const_string("TRELLO_TOKEN")  # токен для доступа к TRELLO
-
