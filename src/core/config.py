c703e03c0f298c42b1feaaab31c14f2cdbb126e0
antonov-andreu <andreu-antonov@yandex.ru>
2022-07-06 03:36:08 +0300

    refactoring def bool and names defs

--- src/core/config.py
+++ src/core/config.py
@@ -12,58 +12,57 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = dotenv_values()
 
 
-def get_const_string(setting: str) -> str:
+def get_string(setting: str) -> str:
     return env.get(setting)
 
 
-def get_const_int(setting: str) -> int:
+def get_int(setting: str) -> int:
     return int(env.get(setting))
 
 
-def get_const_datetime(setting: str) -> datetime:
+def get_datetime(setting: str) -> datetime:
     return datetime.strptime(env.get(setting), "%H:%M")
 
 
-def get_const_datetime_tuple(setting: str) -> tuple:
+def get_datetime_tuple(setting: str) -> tuple:
     return tuple(map(int, list(filter(None, env.get(setting).split(",")))))
 
 
-def get_bool(setting: str):
-    return env.get(setting)
+def get_bool(setting: str) -> bool:
+    if env.get(setting) == "True":
+        return True
+    else:
+        return False
 
 
-LOG_NAME = get_const_string("LOG_NAME")
+LOG_NAME = get_string("LOG_NAME")
 LOG_PATH = BASE_DIR / LOG_NAME
 
-HOST = get_const_string("HOST")  # host для доступа к uvicorn серверу, по умолчанию localhost или 127.0.0.1
-WEBHOOK_URL = get_const_string("WEBHOOK_URL")  # адрес сервера, где будет запущен бот
+HOST = get_string("HOST")  # host для доступа к uvicorn серверу, по умолчанию localhost или 127.0.0.1
+WEBHOOK_URL = get_string("WEBHOOK_URL")  # адрес сервера, где будет запущен бот
 
-PORT = get_const_int("BOT_PORT")  # port для доступа к uvicorn серверу, по умолчанию 8000
-TOKEN = get_const_string("TELEGRAM_TOKEN")  # Токен телеграм бота
+PORT = get_int("BOT_PORT")  # port для доступа к uvicorn серверу, по умолчанию 8000
+TOKEN = get_string("TELEGRAM_TOKEN")  # Токен телеграм бота
 
-WEEKLY_STAT_TIME = get_const_datetime("WEEKLY_STAT_TIME")  # время еженедельной статистики
-WEEKLY_STAT_WEEK_DAYS = get_const_datetime_tuple(
+WEEKLY_STAT_TIME = get_datetime("WEEKLY_STAT_TIME")  # время еженедельной статистики
+WEEKLY_STAT_WEEK_DAYS = get_datetime_tuple(
     "WEEKLY_STAT_WEEK_DAYS"
 )  # дни недели для еженедельной статистики 0-6, где 0 - воскресенье
 
-MONTHLY_STAT_TIME = get_const_datetime("MONTHLY_STAT_TIME")  # время ежемесячной статистики
-MONTHLY_STAT_DAY = get_const_int("MONTHLY_STAT_DAY")  # день для даты ежемесячной статистики
+MONTHLY_STAT_TIME = get_datetime("MONTHLY_STAT_TIME")  # время ежемесячной статистики
+MONTHLY_STAT_DAY = get_int("MONTHLY_STAT_DAY")  # день для даты ежемесячной статистики
 
-MONTHLY_RECEIPT_REMINDER_TIME = get_const_datetime(
+MONTHLY_RECEIPT_REMINDER_TIME = get_datetime(
     "MONTHLY_RECEIPT_REMINDER_TIME"
 )  # время для ежемесячного напоминания о чеке
-MONTHLY_RECEIPT_REMINDER_DAY = get_const_int(
-    "MONTHLY_RECEIPT_REMINDER_DAY"
-)  # день для даты ежемесячного напоминания о чеке
+MONTHLY_RECEIPT_REMINDER_DAY = get_int("MONTHLY_RECEIPT_REMINDER_DAY")  # день для даты ежемесячного напоминания о чеке
 
-URL_SERVICE_RULES = get_const_string("URL_SERVICE_RULES")
+URL_SERVICE_RULES = get_string("URL_SERVICE_RULES")
 
-BOT_PERSISTENCE_FILE = get_const_string("BOT_PERSISTENCE_FILE")  # имя файла persistence бота
-IS_FAKE_API = get_bool(
-    "IS_FAKE_API"
-)  # флаг, определяющий какой АПИ клиент используется - боевой или "заглушка"
-SITE_API_URL = get_const_string("SITE_API_URL")  # адрес сервера, к которому будет отправлять запросы АПИ клиент
+BOT_PERSISTENCE_FILE = get_string("BOT_PERSISTENCE_FILE")  # имя файла persistence бота
+IS_FAKE_API = get_bool("IS_FAKE_API")  # флаг, определяющий какой АПИ клиент используется - боевой или "заглушка"
+SITE_API_URL = get_string("SITE_API_URL")  # адрес сервера, к которому будет отправлять запросы АПИ клиент
 
-TRELLO_API_KEY = get_const_string("TRELLO_API_KEY")  # API ключ разработчика
-TRELLO_ID_MODEL = get_const_string("TRELLO_ID_MODEL")  # id таблицы, к которой будет привязан webhook
-TRELLO_TOKEN = get_const_string("TRELLO_TOKEN")  # токен для доступа к TRELLO
+TRELLO_API_KEY = get_string("TRELLO_API_KEY")  # API ключ разработчика
+TRELLO_ID_MODEL = get_string("TRELLO_ID_MODEL")  # id таблицы, к которой будет привязан webhook
+TRELLO_TOKEN = get_string("TRELLO_TOKEN")  # токен для доступа к TRELLO
