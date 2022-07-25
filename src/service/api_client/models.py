f5281204fd83ed8a19de890ec19bf4078b293bc8
Tony Hat <54199578+t0nyhat@users.noreply.github.com>
2022-07-25 23:25:28 +0300

    update models fields

--- src/service/api_client/models.py
+++ src/service/api_client/models.py
@@ -10,9 +10,9 @@ class UserData:
 
 @dataclass
 class UserMonthStat:
-    closed_consultations: int
-    rating: float
-    average_user_answer_time: float
+    closed_consultations: str
+    rating: str
+    average_user_answer_time: str
 
 
 @dataclass
@@ -46,8 +46,7 @@ class UserActiveConsultations:
 class UserExpiredConsultations:
     username_trello: str
     expired_consultations: int
-    expired_consultation_ids: list[int]
-    expired_consultation_trello_card_ids: list[int]
+    expired_consultations_data: list[dict[str, str]]
 
 
 @dataclass
