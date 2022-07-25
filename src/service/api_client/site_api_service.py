680004c29c2a22548663f1cac65f4188b544dede
Tony Hat <54199578+t0nyhat@users.noreply.github.com>
2022-07-25 23:21:51 +0300

    update site api request functions

--- src/service/api_client/site_api_service.py
+++ src/service/api_client/site_api_service.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 import httpx
 
 from core import config
@@ -50,16 +52,41 @@ class SiteAPIService(AbstractAPIService):
         ...
 
     async def get_user_active_consultations(self, telegram_id: int) -> UserActiveConsultations:
-        ...
+        url = f"{self.site_url}/tgbot/stat/active/user/{telegram_id}"
+        active_consultations = await self.__get_data(url=url)
+        active_consultations = UserActiveConsultations(**active_consultations)
+        return active_consultations
 
     async def get_user_expired_consultations(self, telegram_id: int) -> UserExpiredConsultations:
-        ...
+        url = f"{self.site_url}/tgbot/stat/overdue/user/{telegram_id}"
+        expired_consultations = await self.__get_data(url=url)
+        expired_consultations = UserExpiredConsultations(**expired_consultations)
+        return expired_consultations
 
     async def get_user_month_stat(self, telegram_id: int) -> UserMonthStat:
-        ...
+        url = f"{self.site_url}/tgbot/stat/monthly/user/{telegram_id}"
+        user_month_stat = await self.__get_data(url=url)
+        user_month_stat = UserMonthStat(**user_month_stat)
+        return user_month_stat
 
     async def authenticate_user(self, telegram_id: int) -> UserData | None:
-        ...
+        url = f"{self.site_url}/tgbot/user/{telegram_id}"
+        user = await self.__get_data(url=url)
+        user = UserData(**user)
+        return user
 
-    async def set_user_timezone(self, telegram_id: int, user_time_zone: str) -> httpx:
-        ...
+    async def set_user_timezone(self, telegram_id: int, user_time_zone: str) -> HTTPStatus:
+        url = f"{self.site_url}/tgbot/user"
+        headers = {"Authorization": self.bot_token}
+        data = {"telegram_id": telegram_id, "time_zone": user_time_zone}
+        async with httpx.AsyncClient() as client:
+            response = await client.put(url=url, headers=headers, data=data)
+            return response.status_code
+
+    async def __get_data(self, url: str) -> dict:
+        headers = {"Authorization": self.bot_token}
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url=url, headers=headers)
+            if response.status_code != HTTPStatus.OK:
+                return None
+            return response.json()
