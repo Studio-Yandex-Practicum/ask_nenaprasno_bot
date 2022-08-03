eb6723ea8c43730df450f3af21f2e3c2f0710728
Alexandra <aleksandra.alexand2017@yandex.ru>
2022-08-04 01:09:10 +0300

    fix pep8

--- src/core/config.py
+++ src/core/config.py
@@ -41,6 +41,7 @@ def get_bool(setting: str) -> bool:
 LOG_NAME = get_string("LOG_NAME")
 LOG_PATH = BASE_DIR / LOG_NAME
 
+
 # Параметры локального сервера принимающего обновления от телеграм
 HOST = get_string("HOST")
 WEBHOOK_URL = get_string("WEBHOOK_URL")
