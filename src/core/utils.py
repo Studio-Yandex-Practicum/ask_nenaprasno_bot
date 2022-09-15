21b7b996720ef069fd2f0509095d9a92009b5c06
Igor Shatava <cianoid@yandex.ru>
2022-09-15 22:53:47 +0300

    add functions for building trello's and site's url

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,8 +1,10 @@
 import re
 from datetime import timedelta, timezone
 from typing import Optional
+from urllib.parse import urlencode, urljoin
 
 from constants.timezone import MOSCOW_TIME_OFFSET
+from core.config import TRELLO_BORD_ID, URL_ASK_NENAPRASNO
 
 
 def get_timezone_from_str(tz_string: Optional[str]) -> timezone:
@@ -14,3 +16,16 @@ def get_timezone_from_str(tz_string: Optional[str]) -> timezone:
     sign, hours, minutes = re.search(tz_pattern, tz_string).groups()
     tz_delta = timedelta(hours=int(hours), minutes=int(minutes))
     return timezone(tz_delta) if sign == "+" else timezone(-tz_delta)
+
+
+def build_trello_url(username_trello: str, overdue: bool = False) -> str:
+    trello_filter = [f"member:{username_trello}"]
+    if overdue:
+        trello_filter.append("overdue:true")
+    params = {"filter": ",".join(trello_filter)}
+
+    return "/?".join((urljoin("https://trello.com/", TRELLO_BORD_ID), urlencode(params)))
+
+
+def build_consultation_url(consultation_id: str) -> str:
+    return urljoin(URL_ASK_NENAPRASNO, f"/consultation/redirect/{consultation_id}")
