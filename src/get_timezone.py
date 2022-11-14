6c07142cbe3954dc2e240b5ef888734ae28e392e
Darya <lunaticmoony13@gmail.com>
2022-11-14 12:11:54 +0300

    Returned +13, +14, added check for -13, -14

--- src/get_timezone.py
+++ src/get_timezone.py
@@ -18,7 +18,7 @@ def get_timezone_utc_format(txt_pattern: Optional[str]) -> Optional[datetime.tim
     """Returns datetime.timezone based on string in format UTC+00:00 or NONE"""
     if txt_pattern is None:
         return None
-    tz_pattern = r"^(UTC)?(?P<sign>[-+]?)(?P<hours>(0?[0-9])|(1[0-2]))?(:(?P<minutes>0{1,2}|30|45))?$"
+    tz_pattern = r"^(UTC)?(?P<sign>[-+]?)(?P<hours>(0?[0-9])|(1[0-4]))?(:(?P<minutes>0{1,2}|30|45))?$"
     tz_result = re.search(tz_pattern, txt_pattern, flags=re.IGNORECASE)
     if tz_result is None:
         return None
@@ -31,6 +31,8 @@ def get_timezone_utc_format(txt_pattern: Optional[str]) -> Optional[datetime.tim
     tz_delta = datetime.timedelta(hours=hours, minutes=minutes)
     if tz_result.group("sign") in ("+", ""):
         return datetime.timezone(tz_delta)
+    if hours in [13, 14]:
+        return None
     return datetime.timezone(-tz_delta)
 
 
