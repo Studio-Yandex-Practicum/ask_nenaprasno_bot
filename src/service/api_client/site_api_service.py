56a263e5902894681ea971a6c6583d181971cf83
Igor Shatava <cianoid@yandex.ru>
2022-11-28 07:47:33 +0300

    refactor code (pydantic config feature)

--- src/service/api_client/site_api_service.py
+++ src/service/api_client/site_api_service.py
@@ -5,61 +5,50 @@ from urllib.parse import urljoin
 
 import httpx
 
-from core import config
+from core.config import settings
 from core.logger import log_response, logger
-from service.api_client.base import AbstractAPIService
-from service.api_client.models import (
-    BillStat,
-    Consultation,
-    ConsultationDueDate,
-    MonthStat,
-    UserActiveConsultations,
-    UserData,
-    UserExpiredConsultations,
-    UserMonthStat,
-    WeekStat,
-)
+from service.api_client import base, models
 
 
-class SiteAPIService(AbstractAPIService):
+class SiteAPIService(base.AbstractAPIService):
     def __init__(self):
-        self.site_url: str = config.URL_ASK_NENAPRASNO_API
-        self.bot_token: str = config.SITE_API_BOT_TOKEN
+        self.site_url: str = settings.url_ask_nenaprasno_api
+        self.bot_token: str = settings.site_api_bot_token
 
-    async def get_bill(self) -> Optional[BillStat]:
+    async def get_bill(self) -> Optional[models.BillStat]:
         url = urljoin(self.site_url, "/tgbot/bill")
         users = await self.__get_json_data(url)
         if users is None:
             return None
-        return BillStat.from_dict(users)
+        return models.BillStat.from_dict(users)
 
-    async def get_week_stat(self) -> Optional[list[WeekStat]]:
+    async def get_week_stat(self) -> Optional[list[models.WeekStat]]:
         url = urljoin(self.site_url, "/tgbot/stat/weekly")
         list_week_stat = await self.__get_json_data(url=url)
         if list_week_stat is None:
             return None
-        return [WeekStat.from_dict(one_week_stat) for one_week_stat in list_week_stat]
+        return [models.WeekStat.from_dict(one_week_stat) for one_week_stat in list_week_stat]
 
-    async def get_month_stat(self) -> Optional[list[MonthStat]]:
+    async def get_month_stat(self) -> Optional[list[models.MonthStat]]:
         url = urljoin(self.site_url, "/tgbot/stat/monthly")
         list_month_stat = await self.__get_json_data(url=url)
         if list_month_stat is None:
             return None
-        return [MonthStat.from_dict(one_month_stat) for one_month_stat in list_month_stat]
+        return [models.MonthStat.from_dict(one_month_stat) for one_month_stat in list_month_stat]
 
-    async def get_user_active_consultations(self, telegram_id: int) -> Optional[UserActiveConsultations]:
+    async def get_user_active_consultations(self, telegram_id: int) -> Optional[models.UserActiveConsultations]:
         url = urljoin(self.site_url, f"tgbot/stat/active/user/{telegram_id}")
         active_consultations = await self.__get_json_data(url=url)
         if active_consultations is None:
             return None
-        return UserActiveConsultations.from_dict(active_consultations)
+        return models.UserActiveConsultations.from_dict(active_consultations)
 
-    async def get_user_expired_consultations(self, telegram_id: int) -> Optional[UserExpiredConsultations]:
+    async def get_user_expired_consultations(self, telegram_id: int) -> Optional[models.UserExpiredConsultations]:
         url = urljoin(self.site_url, f"/tgbot/stat/overdue/user/{telegram_id}")
         exp_consultations = await self.__get_json_data(url=url)
         if exp_consultations is None:
             return None
-        return UserExpiredConsultations.from_dict(exp_consultations)
+        return models.UserExpiredConsultations.from_dict(exp_consultations)
 
     async def get_consultations_count(self, telegram_id: int) -> Dict[str, int]:
         """Gets count of active, expiring and expired consultations and returns it in dict.
@@ -73,19 +62,19 @@ class SiteAPIService(AbstractAPIService):
             ).expired_consultations,
         }
 
-    async def get_user_month_stat(self, telegram_id: int) -> Optional[UserMonthStat]:
+    async def get_user_month_stat(self, telegram_id: int) -> Optional[models.UserMonthStat]:
         url = urljoin(self.site_url, f"/tgbot/stat/monthly/user/{telegram_id}")
         user_month_stat = await self.__get_json_data(url=url)
         if user_month_stat is None:
             return None
-        return UserMonthStat.from_dict(user_month_stat)
+        return models.UserMonthStat.from_dict(user_month_stat)
 
-    async def authenticate_user(self, telegram_id: int) -> Optional[UserData]:
+    async def authenticate_user(self, telegram_id: int) -> Optional[models.UserData]:
         url = urljoin(self.site_url, f"/tgbot/user/{telegram_id}")
         user = await self.__get_json_data(url=url)
         if user is None:
             return None
-        return UserData.from_dict(user)
+        return models.UserData.from_dict(user)
 
     async def set_user_timezone(self, telegram_id: int, user_time_zone: str) -> Optional[int]:
         url = urljoin(self.site_url, "/tgbot/user")
@@ -95,19 +84,19 @@ class SiteAPIService(AbstractAPIService):
             response = await client.put(url=url, headers=headers, json=data)
             return response.status_code
 
-    async def get_daily_consultations(self) -> Optional[list[Consultation]]:
+    async def get_daily_consultations(self) -> Optional[list[models.Consultation]]:
         url = urljoin(self.site_url, "tgbot/consultations/")
         consultations = await self.__get_json_data(url=url)
         if consultations is None:
             return None
-        return [Consultation.from_dict(consultation) for consultation in consultations]
+        return [models.Consultation.from_dict(consultation) for consultation in consultations]
 
-    async def get_consultation(self, consultation_id: str) -> Optional[ConsultationDueDate]:
+    async def get_consultation(self, consultation_id: str) -> Optional[models.ConsultationDueDate]:
         url = urljoin(self.site_url, f"tgbot/consultations/{consultation_id}")
         consultation = await self.__get_json_data(url=url)
         if consultation is None:
             return None
-        return ConsultationDueDate.from_dict(consultation)
+        return models.ConsultationDueDate.from_dict(consultation)
 
     async def __get_json_data(self, url: str) -> Optional[dict]:
         headers = {"Authorization": self.bot_token}
